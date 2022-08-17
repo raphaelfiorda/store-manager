cbc5ae399d4a48c982030d6e7b3bdb24e83e1f61
raphaelfiorda <raphaelfiorda@hotmail.com>
2022-08-17 02:35:22 -0300

    docs: Limpa o repositório de arquivos de terceiros, Atualiza o README

--- connection.js
+++ connection.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
+  host: process.env.MYSQL_HOST,
   port: 3306,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
-  database: process.env.MYSQL_DATABASE || 'StoreManager',
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 module.exports = connection;
